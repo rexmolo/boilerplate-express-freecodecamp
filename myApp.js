e3d3d23e75bbfb8f4eaa2d380c13d74a782e5d8e
rexmolo <jackie.hui.ma@gmail.com>
2023-09-07 12:55:34 +0200

    express start

--- myApp.js
+++ myApp.js
@@ -1,5 +1,26 @@
 let express = require('express');
+// const {exec} = require('child_process');
+// const { Webhooks, createNodeMiddleware } = require("@octokit/webhooks");
 let app = express();
+
+// const webhooks = new Webhooks({
+//  secret: "122dfewrrgygde&^&*$)fkdjfoer940%^$(230jfladm"
+// });
+//
+//
+//
+// console.log("hello world");
+//
+//
+app.get('/', (req, res) => {
+ res.send('Hello Express');
+})
+
+// webhooks.on('push', () => {
+//  exec('git pull');
+// });
+
+
 
 
 
